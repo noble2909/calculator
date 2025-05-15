1cef213faf6378bc36d630a78a9a59ba94b1bebd
noble2909 <9148kavinsagar9e@gmail.com>
2025-05-15 11:33:31 +0530

    Calculator working with 2 or more numbers

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function multiply(x,y){
 let number1 
 let number2 
 let operator = "";
+let inputbuffer = "";
 
 function operate(num1,operator,num2){
     if (operator === "+"){
@@ -32,13 +33,8 @@ const display = document.querySelector("#display");
 for (let i=0;i<10;i++){
     const button = document.querySelector(`#button${i}`);
     button.addEventListener("click",() => {
+        inputbuffer+=i;
         display.textContent += i;
-        if (number1 === undefined ){
-            number1 = i;
-        }
-        else{
-            number2 = i;
-        }
         console.log(number1);
         console.log(number2);
     });
@@ -46,34 +42,54 @@ for (let i=0;i<10;i++){
 }
 const mult = document.querySelector("#mult");
 mult.addEventListener("click",() => {
+    if (inputbuffer === "") return; 
+    number1 = parseFloat(inputbuffer); 
     display.textContent += "*";
     operator = "*";
+    inputbuffer=""
     console.log(operator);
 });
 const addition = document.querySelector("#plus");
 addition.addEventListener("click",() => {
+    if (inputbuffer === "") return; 
+    number1 = parseFloat(inputbuffer); 
     display.textContent += "+";
     operator = "+";
+    inputbuffer=""
     console.log(operator);
 });
 const subt = document.querySelector("#subt");
 subt.addEventListener("click",() => {
+    if (inputbuffer === "") return; 
+    number1 = parseFloat(inputbuffer);
     display.textContent += "-";
     operator = "-";
+    inputbuffer=""
     console.log(operator);
 });
 const divid = document.querySelector("#divide");
 divid.addEventListener("click",() => {
+    if (inputbuffer === "") return; 
+    number1 = parseFloat(inputbuffer);
     display.textContent += "/";
     operator = "/";
+    inputbuffer=""
     console.log(operator);
 });
 const equal = document.querySelector("#equal");
 equal.addEventListener("click",() => {
+    if (inputbuffer === "") return; 
+    number2 = parseFloat(inputbuffer);
     let operation = operate(number1,operator,number2);
     display.textContent=operation;
+    inputbuffer = operation.toString();
+    number1 = operation;
+    number2 = undefined;
 });
 const clear = document.querySelector("#clear");
 clear.addEventListener("click",() => {
     display.textContent = " ";
+    number1 = undefined;
+    number2 = undefined;
+    operator = ""
 });
